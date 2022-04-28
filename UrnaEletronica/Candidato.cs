92700d08ebaf9bbf83e71bf4a5c87898481483a5
Luciano Miranda <LucianoDSMiranda@outlook.com>
2022-04-28 20:22:47 -0300

    teste 2

---
+++ UrnaEletronica/Candidato.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrnaEletronica
+{
+    public class Candidato
+    {
+        public string Nome { get; set; }
+        public string Partido { get; set; }
+        public string Sigla { get; set; }
+        public int Id { get; set; }
+
+        public int Votos = 0;   
+         
+
+
+
+    }
+   
+}
